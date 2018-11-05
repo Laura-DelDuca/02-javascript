a7b46fcb39a5a72f72f28bbf811edeabbc5f9269
Laura Del Duca <laura.delduca1990@gmail.com>
2018-11-05 08:48:06 -0500

    ajout exo 1 série 2

--- 02-maths/01-calculator-one/script.js
+++ 02-maths/01-calculator-one/script.js
@@ -12,19 +12,33 @@
 (function() {
     // to get the value of an input: document.getElememntById("element-id").value
 
+
+
     document.getElementById("addition").addEventListener("click", function() {
         // perform an addition
+        var one = document.getElementById("op-one").value;
+        var two = document.getElementById("op-two").value;
+        alert(parseInt (one) + parseInt (two));
     });
 
     document.getElementById("substraction").addEventListener("click", function() {
         // perform an substraction
+        var one = document.getElementById ("op-one").value
+        var two = document.getElementById ("op-two").value
+        alert(parseInt (one) - parseInt (two));
     });
 
     document.getElementById("multiplication").addEventListener("click", function() {
         // perform an multiplication
+        var one = document.getElementById ("op-one").value
+        var two = document.getElementById ("op-two").value
+        alert(parseInt (one) * parseInt (two));
     });
 
     document.getElementById("division").addEventListener("click", function() {
         // perform an division
+        var one = document.getElementById ("op-one").value
+        var two = document.getElementById ("op-two").value
+        alert(parseInt (one) / parseInt (two));
     });
 })();
