d5d85bb8b9c4913c33e336a782461da0144babcd
Laura Del Duca <laura.delduca1990@gmail.com>
2018-11-06 09:40:04 -0500

    ajout exo 3 série 3

--- 03-colors/03-change-bcg-three/script.js
+++ 03-colors/03-change-bcg-three/script.js
@@ -13,4 +13,16 @@
 
     // your code here
 
+    document.getElementById("run").addEventListener("click", function(){
+    var randomColor1 = Math.floor(Math.random() * 255);
+    var randomColor2 = Math.floor(Math.random() * 255);
+    var randomColor3 = Math.floor(Math.random() * 255);
+
+    document.documentElement.style.backgroundColor= "rgb("+randomColor1+"," +randomColor2+"," +randomColor3+")"
+    
+})
+
+
+
+
 })();
