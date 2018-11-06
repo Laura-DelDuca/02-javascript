a8576fc244bbabc9ce76040228cdc8e00fa30d10
Laura Del Duca <laura.delduca1990@gmail.com>
2018-11-06 10:11:43 -0500

    ajout exo 1 série 4

--- 04-dates/01-title-by-hour-one/script.js
+++ 04-dates/01-title-by-hour-one/script.js
@@ -14,5 +14,17 @@
     // to change the content of a tag: document.getElementById("element-id").innerHTML = "new-value"
 
     // your code here
+   
+    var date = new Date ();
+    var hour = date.getHours();
+    var message;
 
+    if (hour <= 18)
+        message= "Bonjour";
+    else if (hour > 18)
+        message= "Bonsoir";
+
+    document.getElementById("target").innerHTML = message;
+
+    
 })();
