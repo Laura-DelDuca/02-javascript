9430fcd5c8aa366d76ca60f70c00537de18faac4
Laura Del Duca <laura.delduca1990@gmail.com>
2018-11-06 08:40:19 -0500

    ajout exo 2 série 3

--- 03-colors/02-change-bcg-two/script.js
+++ 03-colors/02-change-bcg-two/script.js
@@ -12,5 +12,11 @@
 (function() {
 
     // your code here
+    
+
+    document.getElementById("run").addEventListener("click", function(){
+        var pageColor = document.getElementById("color").value;
+        document.documentElement.style.backgroundColor= pageColor;
+    })
 
 })();
