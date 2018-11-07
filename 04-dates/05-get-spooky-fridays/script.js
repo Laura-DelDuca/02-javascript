9e75d47232cc247eae6f737d4adabb129c0b30b1
Laura Del Duca <laura.delduca1990@gmail.com>
2018-11-07 14:19:31 +0100

    ajout exo5 série 4

--- 04-dates/05-get-spooky-fridays/script.js
+++ 04-dates/05-get-spooky-fridays/script.js
@@ -13,4 +13,26 @@
 
     // your code here
 
+    document.getElementById("run").addEventListener("click", function(){
+
+        var date = new Date();
+        var month;
+        var mois = new Array("janvier", "fevrier", "mars", "avril", "mai", "juin", "juillet", "aout", "septembre", "octobre", "novembre", "decembre");
+        var friday13="";
+        var year = document.getElementById("year").value;
+
+        for(month = 0 ; month < 12; month++){
+              
+            date = new Date(year,month,13,8,0,0);
+            console.log(date);
+                if (date.getDay() === 5)
+                    {
+                    friday13 += mois[month]+" ";
+                    }                 
+    }
+
+    alert(friday13)
+
+    })
+       
 })();
