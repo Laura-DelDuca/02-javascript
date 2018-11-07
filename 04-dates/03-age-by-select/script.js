e8c5ce3562bcbb3876f1eb37b3b425e95b8cea96
Laura Del Duca <laura.delduca1990@gmail.com>
2018-11-07 10:15:40 +0100

    ajout exo 3 série 4

--- 04-dates/03-age-by-select/script.js
+++ 04-dates/03-age-by-select/script.js
@@ -13,4 +13,25 @@
 
     // your code here
 
+
+    document.getElementById("run").addEventListener("click", function (){
+
+        var day = document.getElementById("dob-day").value;
+        var month = document.getElementById("dob-month").value;
+        var year = document.getElementById("dob-year").value;
+    
+        var birthday = new Date (year+ "/" +month+ "/" +day);
+        var today = new Date ();
+    
+        var years = today.getFullYear () - birthday.getFullYear ();
+        console.log(today - birthday);   
+        
+        birthday.setFullYear(today.getFullYear());
+    
+        if (today < birthday) {
+            years--; 
+        }
+        
+        alert("Tu as " +years+ " ans");
+    })
 })();
