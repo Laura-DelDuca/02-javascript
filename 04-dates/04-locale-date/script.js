f7eb43a4a72713ab53b63535543f27ab9d660866
Laura Del Duca <laura.delduca1990@gmail.com>
2018-11-07 11:41:08 +0100

    ajout exo 4 série 4

--- 04-dates/04-locale-date/script.js
+++ 04-dates/04-locale-date/script.js
@@ -15,4 +15,24 @@
 
     // your code here
 
+        var jours = new Array("dimanche", "lundi", "mardi", "mercredi", "jeudi", "vendredi", "samedi");
+        var mois = new Array("janvier", "fevrier", "mars", "avril", "mai", "juin", "juillet", "aout", "septembre", "octobre", "novembre", "decembre");
+        var date = new Date();
+    
+        var dateFrs = jours[date.getDay()] + " ";
+        dateFrs += date.getDate() + " ";
+        dateFrs += mois[date.getMonth()] + " ";
+    
+        var heure = date.getHours();
+        var minutes = date.getMinutes();
+        var secondes = date.getSeconds();
+    
+        var hours = heure + ":";
+        hours += minutes + ":";
+        hours += secondes;
+    
+    
+    
+        document.getElementById("target").innerHTML = dateFrs + hours
+
 })();
