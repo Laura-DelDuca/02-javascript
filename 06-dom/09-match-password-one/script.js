f489d85147f01c42f4b42683462af12563d21f95
Laura Del Duca <laura.delduca1990@gmail.com>
2018-11-12 16:23:52 +0100

    ajout exo 9 série 6

--- 06-dom/09-match-password-one/script.js
+++ 06-dom/09-match-password-one/script.js
@@ -10,7 +10,15 @@
 // You will have time to focus on it later.
 
 (function() {
+    document.getElementById("run").addEventListener("click", function() {
+        if(document.getElementById("pass-one").value != document.getElementById("pass-two").value) {
 
-    // your code here
+                //border red
+                document.getElementById("pass-one").style.borderColor = "red";
+                document.getElementById("pass-two").style.borderColor = "red";
+               
+        }
+    });
+
 
 })();
