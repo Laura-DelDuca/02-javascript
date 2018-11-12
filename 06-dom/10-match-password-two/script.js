8a7f50d48531ed383b26538871f26ed81063c5a7
Laura Del Duca <laura.delduca1990@gmail.com>
2018-11-12 16:29:31 +0100

    ajout exo 10 série 6

--- 06-dom/10-match-password-two/script.js
+++ 06-dom/10-match-password-two/script.js
@@ -11,6 +11,14 @@
 
 (function() {
 
-    // your code here
+    // Yet to check...
+    document.getElementById("run").addEventListener("click", function() {
 
+        if(document.getElementById("pass-one").value != document.getElementById("pass-two").value) {
+
+            document.getElementById("pass-one").setAttribute("class", "ERROR");
+
+            document.getElementById("pass-two").setAttribute("class", "ERROR");
+        }
+    });
 })();
