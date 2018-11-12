95e218e7dd6e2a340208a16fde5eee632297bcb9
Laura Del Duca <laura.delduca1990@gmail.com>
2018-11-12 15:27:55 +0100

    ajout exo 7 série 6

--- 06-dom/07-generate-table-one/script.js
+++ 06-dom/07-generate-table-one/script.js
@@ -13,4 +13,15 @@
 
     // your code here
 
+var table = document.createElement("table");
+var tr;
+
+for (var i = 0; i < 10; i++) {
+    tr = document.createElement ("tr");
+    tr.appendChild(document.createElement ("td"));
+    table.appendChild(tr);
+}
+
+document.getElementById("target").appendChild(table);
+
 })();
