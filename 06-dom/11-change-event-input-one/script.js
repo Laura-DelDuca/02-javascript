3fe8adafd2335045cfe05e5a558982d9a584838d
Laura Del Duca <laura.delduca1990@gmail.com>
2018-11-12 16:44:49 +0100

    ajout exo 11 série 6

--- 06-dom/11-change-event-input-one/script.js
+++ 06-dom/11-change-event-input-one/script.js
@@ -13,4 +13,13 @@
 
     // your code here
 
+    var i = 0;
+    document.getElementById("pass-one").addEventListener("input", function() {
+        i++;
+
+        if(i === 10) 
+            document.getElementById("pass-one").disabled = true;
+        if(i <= 10) 
+            document.getElementById("counter").innerHTML = i + "/10";
+    });
 })();
