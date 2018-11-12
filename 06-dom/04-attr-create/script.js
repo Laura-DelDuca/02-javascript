5ff0746eb807fbcdc7659b193b553f8a7f8794ec
Laura Del Duca <laura.delduca1990@gmail.com>
2018-11-12 10:09:41 +0100

    ajout exo4 série 6

--- 06-dom/04-attr-create/script.js
+++ 06-dom/04-attr-create/script.js
@@ -13,4 +13,9 @@
 
     // your code here
 
-})();
+    var img = document.createElement("img"); 
+    img.setAttribute("src", document.getElementById("source").getAttribute("data-image")); 
+    document.getElementById("target").appendChild(img); 
+    document.getElementById("source").remove();
+
+})()
