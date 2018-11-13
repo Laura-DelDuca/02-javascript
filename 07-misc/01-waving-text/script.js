a3018e47e99d86eea2c8844de0eedced3b5c1eab
Laura Del Duca <laura.delduca1990@gmail.com>
2018-11-13 15:43:53 +0100

    ajout exo 1 série 7

--- 07-misc/01-waving-text/script.js
+++ 07-misc/01-waving-text/script.js
@@ -9,8 +9,32 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
+
 (function() {
+    //variables
+    var wave = document.getElementById("target").innerHTML;
+    var myText = "";
+    var textSize = [14, 16, 18, 20, 22, 24, 26, 24, 22, 20, 18, 16, 14];
 
-    // your code here
+    // loop?
+    for(var i = 0; i < wave.length; i++) {
+        if(wave[i] != " ") myText += '<span style="font-size:' + textSize[i % 9] + 'px;">' + wave[i] + '</span>';
+        else myText += " ";
 
+    }
+    document.getElementById("target").innerHTML = myText;
 })();
+
+
+
+// First try... Screwed up
+/*(function() {
+    var wave = document.getElementById("target").innerHTML;
+    var myText = "";
+    var size = [10, 12, 14, 16, 18, 16, 14, 12, 10];
+    for(var i = 0; i < wave.length; i++) {
+        if(wave[i] != " ") myText += <span style="font-size: + size + px;"> + wave[i] + </span>;
+
+    }
+    document.getElementById("target").innerHTML = myText;
+    */
