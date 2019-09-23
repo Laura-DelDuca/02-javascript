92373c541f2221ec505614276bc48e085a7b2217
Laura <l.delduca@hotmail.com>
2019-09-23 11:48:49 +0200

    Refactor code in maths folder

--- 02-maths/01-calculator-one/script.js
+++ 02-maths/01-calculator-one/script.js
@@ -1,19 +1,5 @@
-/* becode/javascript
- *
- * /02-maths/01-calculator-one/script.js - 2.1: calculatrice
- *
- * coded by leny@BeCode
- * started at 26/10/2018
- */
-
-// NOTE: don't focus on the existing code structure for now.
-// You will have time to focus on it later.
-
 (function() {
-    // to get the value of an input: document.getElememntById("element-id").value
-
-
-
+    // to get the value of an input: document.getElementById("element-id").value
     document.getElementById("addition").addEventListener("click", function() {
         // perform an addition
         var one = document.getElementById("op-one").value;
@@ -22,21 +8,21 @@
     });
 
     document.getElementById("substraction").addEventListener("click", function() {
-        // perform an substraction
+        // perform a substraction
         var one = document.getElementById ("op-one").value
         var two = document.getElementById ("op-two").value
         alert(parseInt (one) - parseInt (two));
     });
 
     document.getElementById("multiplication").addEventListener("click", function() {
-        // perform an multiplication
+        // perform a multiplication
         var one = document.getElementById ("op-one").value
         var two = document.getElementById ("op-two").value
         alert(parseInt (one) * parseInt (two));
     });
 
     document.getElementById("division").addEventListener("click", function() {
-        // perform an division
+        // perform a division
         var one = document.getElementById ("op-one").value
         var two = document.getElementById ("op-two").value
         alert(parseInt (one) / parseInt (two));
--- 02-maths/03-even-square/script.js
+++ 02-maths/03-even-square/script.js
@@ -1,25 +1,15 @@
-/* becode/javascript
- *
- * /02-maths/03-even-square/script.js - 2.3: carrés des pairs
- *
- * coded by leny@BeCode
- * started at 26/10/2018
- */
-
-// NOTE: don't focus on the existing code structure for now.
-// You will have time to focus on it later.
-
 (function() {
 
     document.getElementById("run").addEventListener("click", function() {
 
-        // your code here
-
         var result = "";
 
-        for(var i=0 ; i<21 ; i++ ) {
+        for(var i=1 ; i<21 ; i++ ) {
             if (i%2 == 0) {
-            result += "le carré de "+i+" est : "+Math.pow (i,2) + "\n";
+            // The modulo operation finds the remainder after division of one number by another 
+            // Ici, avec modulo 2, on s'assure de ne garder que les nombres pairs dans la boucle entre 1 et 21   
+            result += "Le carré de "+i+" est : "+Math.pow (i,2) + "\n";
+            // Math.pow() envoie un nombre à une certaine puissance.
             }
         }
 
